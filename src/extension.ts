cc709893ba417d841d9c4c3e4badd0998175e481
ilimei <514222057@qq.com>
2022-11-22 21:34:06 +0800

    feat: support multi trans

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 import { activateBarEntry } from './barEntry';
 import EditorProvider from './EditorProvider';
 import PreviewGetter from './editors';
 import blp2Image from './command/blp2img';
 import { mdl2mdx, mdx2mdl } from './command/mdl2mdx';
-import * as nls from 'vscode-nls';
-const localize = nls.loadMessageBundle();
+import { init, localize } from './localize';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	const edit = new vscode.WorkspaceEdit();
+	init(context.extensionPath);
 
 	const { sizeStatusBarEntry, binarySizeStatusBarEntry, zoomStatusBarEntry } = activateBarEntry(context);
 
@@ -28,24 +28,87 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('blpPreview.convert2png', async (uri: vscode.Uri, selectURI: vscode.Uri[]) => {
-		const distPath = uri.with({ path: uri.path + '.png' });
-		edit.createFile(distPath, { ignoreIfExists: true });
-		blp2Image(uri.fsPath, distPath.fsPath, 'png');
-		await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+		if (selectURI.length > 1) {
+			vscode.window.showOpenDialog({
+				canSelectMany: false,
+				openLabel: localize('blpPreview.saveBlpFolder', 'Select'),
+				canSelectFiles: false,
+				canSelectFolders: true,
+			}).then(folders => {
+				if (folders && folders[0]) {
+					for (let uri of selectURI) {
+						if (!uri.fsPath.toLocaleLowerCase().endsWith('.blp')) {
+							continue;
+						}
+						const distPath = folders[0].with({ path: folders[0].path + '/' + path.basename(uri.fsPath) + '.png' });
+						edit.createFile(distPath, { ignoreIfExists: true });
+						blp2Image(uri.fsPath, distPath.fsPath, 'png');
+					}
+					return vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+				}
+			});
+		} else {
+			const distPath = uri.with({ path: uri.path + '.png' });
+			edit.createFile(distPath, { ignoreIfExists: true });
+			blp2Image(uri.fsPath, distPath.fsPath, 'png');
+			await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+		}
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('blpPreview.convert2jpg', async (uri: vscode.Uri, selectURI: vscode.Uri[]) => {
-		const distPath = uri.with({ path: uri.path + '.jpg' });
-		edit.createFile(distPath, { ignoreIfExists: true });
-		blp2Image(uri.fsPath, distPath.fsPath, 'jpg');
-		await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+		if (selectURI.length > 1) {
+			vscode.window.showOpenDialog({
+				canSelectMany: false,
+				openLabel: localize('blpPreview.saveBlpFolder', 'Select'),
+				canSelectFiles: false,
+				canSelectFolders: true,
+			}).then(folders => {
+				if (folders && folders[0]) {
+					for (let uri of selectURI) {
+						if (!uri.fsPath.toLocaleLowerCase().endsWith('.blp')) {
+							continue;
+						}
+						const distPath = folders[0].with({ path: folders[0].path + '/' + path.basename(uri.fsPath) + '.jpg' });
+						edit.createFile(distPath, { ignoreIfExists: true });
+						blp2Image(uri.fsPath, distPath.fsPath, 'jpg');
+					}
+					return vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+				}
+			});
+		} else {
+			const distPath = uri.with({ path: uri.path + '.jpg' });
+			edit.createFile(distPath, { ignoreIfExists: true });
+			blp2Image(uri.fsPath, distPath.fsPath, 'jpg');
+			await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+		}
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('blpPreview.convert2blp', async (uri: vscode.Uri, selectURI: vscode.Uri[]) => {
-		const distPath = uri.with({ path: uri.path + '.blp' });
-		edit.createFile(distPath, { ignoreIfExists: true });
-		blp2Image(uri.fsPath, distPath.fsPath, 'blp');
-		await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+		if (selectURI.length > 1) {
+			vscode.window.showOpenDialog({
+				canSelectMany: false,
+				openLabel: localize('blpPreview.saveBlpFolder', 'Select'),
+				canSelectFiles: false,
+				canSelectFolders: true,
+			}).then(folders => {
+				if (folders && folders[0]) {
+					for (let uri of selectURI) {
+						if (!uri.fsPath.toLocaleLowerCase().endsWith('.jpg') && !uri.fsPath.toLocaleLowerCase().endsWith('.png')) {
+							continue;
+						}
+						const distPath = folders[0].with({ path: folders[0].path + '/' + path.basename(uri.fsPath) + '.blp' });
+						edit.createFile(distPath, { ignoreIfExists: true });
+						blp2Image(uri.fsPath, distPath.fsPath, 'blp');
+					}
+					return vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+				}
+			});
+		} else {
+			const distPath = uri.with({ path: uri.path + '.blp' });
+			edit.createFile(distPath, { ignoreIfExists: true });
+			blp2Image(uri.fsPath, distPath.fsPath, 'blp');
+			await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+		}
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('blpPreview.convert2mdl', async (uri: vscode.Uri, selectURI: vscode.Uri[]) => {
