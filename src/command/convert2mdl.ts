7206f6596f8f07fe1c841ec8e62d0c5d1ae8b9dc
TheMe <sdqvsqiu@gmail.com>
2023-07-18 14:45:35 +0800

    add: mdx/mdl批量转换 (#14)

--- src/command/convert2mdl.ts
+++ src/command/convert2mdl.ts
@@ -7,14 +7,36 @@ import { mdx2mdl } from './helper/mdl2mdx';
 commandMap.set('blpPreview.convert2mdl', async function (uri: vscode.Uri, selectURI: vscode.Uri[]) {
     const data = vscode.workspace.getConfiguration("convert2mdl");
     const shouldReplaceExt = data && data.convert2mdl ? data.convert2mdl : false;
-    try {
-        const distPath = uri.with({ path: shouldReplaceExt ? uri.path.replace(/\.(mdx)$/i, '.mdl') : uri.path + '.mdl' });
-        this.edit.createFile(distPath, { ignoreIfExists: true });
-        const buf = await vscode.workspace.fs.readFile(uri);
-        mdx2mdl(buf, distPath.fsPath);
-        await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
-    } catch (e) {
-        console.error(e);
+
+    if (selectURI.length > 1) {
+        vscode.window.showOpenDialog({
+            canSelectMany: false,
+            openLabel: localize('blpPreview.saveBlpFolder', 'Select'),
+            canSelectFiles: false,
+            canSelectFolders: true,
+        }).then(async folders => {
+            if (folders && folders[0]) {
+                for (let uri of selectURI) {
+                    if (!uri.fsPath.toLocaleLowerCase().endsWith('.mdx')) {
+                        continue;
+                    }
+                    const distPath = folders[0].with({ path: folders[0].path + '/' + path.basename(uri.fsPath) + '.mdl' });
+                    this.edit.createFile(distPath, { ignoreIfExists: true });
+                    const buf = await vscode.workspace.fs.readFile(uri);
+                    mdx2mdl(buf, distPath.fsPath);
+                }
+                return vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+            }
+        });
+    } else {
+        try {
+            const distPath = uri.with({ path: shouldReplaceExt ? uri.path.replace(/\.(mdx)$/i, '.mdl') : uri.path + '.mdl' });
+            this.edit.createFile(distPath, { ignoreIfExists: true });
+            const buf = await vscode.workspace.fs.readFile(uri);
+            mdx2mdl(buf, distPath.fsPath);
+            await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+        } catch (e) {
+            console.error(e);
+        }
     }
 });
-
--- src/command/convert2mdx.ts
+++ src/command/convert2mdx.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import * as vscode from 'vscode';
 import { localize } from '../localize';
 import commandMap from "./helper/commands";
@@ -6,9 +7,34 @@ import { mdl2mdx } from './helper/mdl2mdx';
 commandMap.set('blpPreview.convert2mdx', async function (uri: vscode.Uri, selectURI: vscode.Uri[]) {
     const data = vscode.workspace.getConfiguration("convert2mdx");
     const shouldReplaceExt = data && data.convert2mdx ? data.convert2mdx : false;
-    const distPath = uri.with({ path: shouldReplaceExt ? uri.path.replace(/\.(mdl)$/i, '.mdx') : uri.path + '.mdx' });
-    this.edit.createFile(distPath, { ignoreIfExists: true });
-    mdl2mdx(uri.fsPath, distPath.fsPath);
-    await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
-});
 
+    if (selectURI.length > 1) {
+        vscode.window.showOpenDialog({
+            canSelectMany: false,
+            openLabel: localize('blpPreview.saveBlpFolder', 'Select'),
+            canSelectFiles: false,
+            canSelectFolders: true,
+        }).then(folders => {
+            if (folders && folders[0]) {
+                for (let uri of selectURI) {
+                    if (!uri.fsPath.toLocaleLowerCase().endsWith('.mdl')) {
+                        continue;
+                    }
+                    const distPath = folders[0].with({ path: folders[0].path + '/' + path.basename(uri.fsPath) + '.mdx' });
+                    this.edit.createFile(distPath, { ignoreIfExists: true });
+                    mdl2mdx(uri.fsPath, distPath.fsPath);
+                }
+                return vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+            }
+        });
+    } else {
+        try {
+            const distPath = uri.with({ path: shouldReplaceExt ? uri.path.replace(/\.(mdl)$/i, '.mdx') : uri.path + '.mdx' });
+            this.edit.createFile(distPath, { ignoreIfExists: true });
+            mdl2mdx(uri.fsPath, distPath.fsPath);
+            await vscode.window.showInformationMessage(localize("blpPreview.convertSuccess", "convert success"));
+        } catch (e) {
+            console.error(e);
+        }
+    }
+});
